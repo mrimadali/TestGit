ef400188338fc10526efde99f104247598f66f8a
Adel Al Tamimi <adel.altamimi@dewa.gov.ae>
2016-05-09 11:21:45 +0400

    Initial Commit

---
+++ TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Adel Al Tamimi on 5/9/16.
+//  Copyright © 2016 DEWA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by Adel Al Tamimi on 5/9/16.
+//  Copyright © 2016 DEWA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
